c66dc36ad82ac7e5afaea4b8b3edc10048edbe99
akhil-kat <akhil.s@katzion.com>
2020-03-09 16:40:19 +0530

    Code push

---
+++ src/com/java/pack/Stringconcant.java
@@ -0,0 +1,13 @@
+package com.java.pack;
+
+public class Stringconcant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String firstName="Akhil";
+String secondName="Somashekaran";
+
+System.out.println(firstName+ " "+secondName);
+	}
+
+}
---
+++ src/com/java/pack/testclass.java
@@ -0,0 +1,14 @@
+package com.java.pack;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print whatever 
+		/* test mulitples line commesnt
+		 * for testing multiline comment
+		 */
+
+	}
+
+}
